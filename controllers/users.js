fd55c9692e73322b84f4e552d72f7a7ec486a31e
Svyatoslav <svyatoslav05j@gmail.com>
2022-02-24 16:22:31 +0300

    Проект создан

--- controllers/users.js
+++ controllers/users.js
@@ -1,81 +1,81 @@
-const User = require('../models/user');
-
+const User = require('../models/user')
+// Обработка ошибок в асинхронном коде. Async/await
 exports.getUsers = async (req, res) => {
   try {
-    const users = await User.find({});
+    // Метод find возвращает все документы по запросу
+    const users = await User.find({})
     res.status(200).send(users);
-  } catch (err) {
-    res.status(500).send({ message: 'На сервере произошла ошибка', ...err });
+  }catch(err){
+    res.status(500).send('Ошибка по умолчанию.');
   }
-};
+}
 
-exports.getUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.userId);
-    if (user) {
-      return res.status(200).send(user);
+exports.getUserById = async(req, res) =>{
+  try{
+    // Метод findById возвращает документы найденные по id
+    const user = await User.findById(req.params.userId)
+    if(user){
+      res.status(200).send(user);
+    }else{
+      res.status(404).send({message: 'Пользователь по указанному _id не найден.'});
     }
-    return res.status(404).send({ message: 'Пользователя с таким id не существует' });
-  } catch (err) {
-    if (err.name === 'CastError') {
-      return res.status(400).send({ message: 'Переданы некорректные данные' });
+  }catch(err){
+    if (err.name = 'ValidatorError') {
+      res.status(400).send({message: 'Некорректно переданы данные пользователя'})
+    } else {
+      res.status(500).send('Ошибка по умолчанию.');
     }
-    return res.status(500).send({ message: 'На сервере произошла ошибка', ...err });
   }
-};
+}
 
 exports.createUser = async (req, res) => {
   try {
-    const newUser = new User(req.body);
-    return res.status(201).send(await newUser.save());
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      return res.status(400).send({ message: 'Произошла ошибка при заполнении обязательных полей' });
+    const { name, about, avatar } = req.body;
+    const user = await User.create({ name, about, avatar });
+    if (user) {
+      res.status(201).send(user);
     }
-    return res.status(500).send({ message: 'На сервере произошла ошибка', ...err });
+  }catch(err){
+    if (err.name = 'ValidatorError') {
+      res.status(400).send({message: 'Переданы некорректные данные при создании пользователя.'})
+    } else {
+      res.status(500).send({message: 'Ошибка по умолчанию.'})
+    }
   }
-};
+}
 
-exports.updateUser = async (req, res) => {
+exports.updateUser  = async (req, res) => {
   try {
-    const user = await User.findByIdAndUpdate(
-      req.user._id,
-      { name: req.body.name, about: req.body.about },
-      {
-        new: true,
-        runValidators: true,
-      },
-    );
+    const { name, about } = req.body;
+    const user = await User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     if (user) {
       return res.status(200).send(user);
+    }else{
+      return res.status(404).send({message: 'Пользователь с указанным _id не найден.'});
     }
-    return res.status(404).send({ message: 'Пользователь не найден' });
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      return res.status(400).send({ message: 'Произошла ошибка при заполнении обязательных полей' });
+  }catch(err){
+    if(err.name === 'ValidatorError') {
+      res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+    } else {
+      res.status(500).send({ message: 'Ошибка по умолчанию.' });
     }
-    return res.status(500).send({ message: 'На сервере произошла ошибка', ...err });
   }
-};
+}
 
-exports.updateAvatar = async (req, res) => {
+exports.updateUserAvatar  = async (req, res) => {
   try {
-    const user = await User.findByIdAndUpdate(
-      req.user._id,
-      { avatar: req.body.avatar },
-      {
-        new: true,
-        runValidators: true,
-      },
-    );
-    if (user) {
-      return res.status(200).send(user);
+    const { avatar } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    if (updatedUser) {
+      res.status(200).send(updatedUser);
+    }else{
+      res.status(404).send({message: 'Пользователь с указанным _id не найден.'});
     }
-    return res.status(404).send({ message: 'Пользователь не найден' });
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      return res.status(400).send({ message: 'Произошла ошибка при заполнении обязательных полей' });
+  }catch(err){
+    if(err.name === 'ValidatorError') {
+      res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара.' });
+    } else {
+      res.status(500).send({ message: 'Ошибка по умолчанию.' });
     }
-    return res.status(500).send({ message: 'На сервере произошла ошибка', ...err });
   }
-};
+}
