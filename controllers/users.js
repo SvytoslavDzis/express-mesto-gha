ae2de3faa00bf70f5557c7e33ad10a07ef6b9382
Svyatoslav <Svyatoslav5j@gmail.com>
2022-02-24 20:28:52 +0300

    Проект №13.1

--- controllers/users.js
+++ controllers/users.js
@@ -64,8 +64,7 @@ exports.updateUser  = async (req, res) => {
 
 exports.updateUserAvatar  = async (req, res) => {
   try {
-    const { avatar } = req.body;
-    const updatedUser = await User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    const updatedUser = await User.findByIdAndUpdate(req.user._id, { name: req.body.name, about: req.body.about }, { new: true, runValidators: true })
     if (updatedUser) {
       res.status(200).send(updatedUser);
     }else{
